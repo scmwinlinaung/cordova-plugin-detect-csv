db8d092fea00d442346ab485e56bd1a0b8de78ca
winlinaung <scm.winlinaung@gmail.com>
2019-12-16 15:24:11 +0630

    adding_document_class

--- src/ios/detect_csv.swift
+++ src/ios/detect_csv.swift
@@ -2,10 +2,18 @@
 * Notes: The @objc shows that this class & function should be exposed to Cordova.
 */
 import UIKit
-var fileUrl: String
+var fileUrl: String?
 @objc(detect_csv) class detect_csv : CDVPlugin {
   @objc(getFileUrl:) // Declare your function name.
   func getFileUrl(command: CDVInvokedUrlCommand) { // write the function code.
+    var document: Document?
+    document?.open(completionHandler: { (success) in
+         if success {
+             if self.document?.fileType == "public.comma-separated-values-text" {
+                 fileUrl = self.document?.fileURL
+             }
+         }
+    })
     /* 
      * Always assume that the plugin will fail.
      * Even if in this example, it can't.
@@ -13,7 +21,7 @@ var fileUrl: String
     // Set the plugin result to fail.
     var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
     // Set the plugin result to succeed.
-        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: fileUrl);
     // Send the function result back to Cordova.
     self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
   }
